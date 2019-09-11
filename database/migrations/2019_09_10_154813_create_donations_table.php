fea7bafc04014994caa90a7a0d011de90b8c732d
yudi <yudi@technopartner.id>
2019-09-11 23:27:57 +0700

    [Added] create donations migrations

--- database/migrations/2019_09_10_154813_create_donations_table.php
+++ database/migrations/2019_09_10_154813_create_donations_table.php
@@ -14,7 +14,14 @@ class CreateDonationsTable extends Migration
     public function up()
     {
         Schema::create('donations', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('donor_name')->nullable();
+            $table->string('donor_email')->nullable();
+            $table->string('donation_type')->nullable();
+            $table->decimal('amount', 20, 2)->default(0);
+            $table->string('note')->nullable();
+            $table->string('status')->default('pending');
+            $table->string('snap_token')->nullable();
             $table->timestamps();
         });
     }
